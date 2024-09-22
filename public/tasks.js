bf910815c12985dd3d96c62875b6937e56c8c344
Rojauhn <146143513+Rojauhn@users.noreply.github.com>
2024-09-21 22:24:06 -0400

    initial commit

---
+++ public/tasks.js
@@ -0,0 +1,7 @@
+const tasksData = [
+  { "id": 1, "task": "Complete Next.js tutorial", "done": false },
+  { "id": 2, "task": "Read documentation", "done": false },
+  { "id": 3, "task": "Start a new project", "done": false }
+]
+
+export default tasksData
